ce1b34a6c4c5e7b47e2e2f18d20f07e7208ee61b
uvaldez <uzielvaldez85@gmail.com>
2024-05-12 17:43:28 -0500

    fix: contants file update

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
-// export const FOOD_TRUCKS_BASE_URL = 'https://foodtrucks-api-04qa.onrender.com'; // replace with http://localhost:8080 for running local api
-export const FOOD_TRUCKS_BASE_URL = 'http://localhost:8080'; // replace with http://localhost:8080 for running local api
+export const FOOD_TRUCKS_BASE_URL = 'https://foodtrucks-api-04qa.onrender.com'; // replace with http://localhost:8080 for running local api
 
 export const PAGINATION_LIMIT = 5;
